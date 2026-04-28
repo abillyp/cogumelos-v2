1bb7e17756c150547a38a3677f4094892e7ca85b
Billy <abillyp@gmail.com>
2026-04-28 13:10:51 -0300

    correcao update constraint

--- backend/src/main/resources/db/migration/V5__update_plano_check.sql
+++ backend/src/main/resources/db/migration/V5__update_plano_check.sql
@@ -1,3 +1,3 @@
-ALTER TABLE tenants DROP CONSTRAINT tenants_plano_check;
+ALTER TABLE tenants DROP CONSTRAINT IF EXISTS tenants_plano_check;
 ALTER TABLE tenants ADD CONSTRAINT tenants_plano_check
     CHECK (plano IN ('BASICO', 'PRO', 'COMERCIAL'));
