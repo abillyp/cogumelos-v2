972dee39969c2014d3cb272fa56ff53e58da609e
Billy <abillyp@gmail.com>
2026-05-14 11:56:15 -0300

    fix: update insumo

--- backend/src/main/resources/db/migration/V10_update_insumos.sql
+++ backend/src/main/resources/db/migration/V10_update_insumos.sql
@@ -1,2 +1,6 @@
-INSERT INTO insumos (id, nome, mo_pct, carbono_pct, nitrogenio_pct , cn_ratio , ph , categoria , tenant_id ) values
-    ('ins-15', 'Gesso', 0,0,0, null , null, 'Resíduo' , 1);
+INSERT INTO insumos (id, nome, mo_pct, carbono_pct, nitrogenio_pct, cn_ratio, ph, categoria, tenant_id)
+SELECT 'ins-15', 'Gesso', 0, 0, 0, null, null, 'Resíduo',
+       (SELECT id FROM tenants WHERE email = 'sistema@cogumelos.app')
+WHERE NOT EXISTS (
+    SELECT 1 FROM insumos WHERE id = 'ins-15'
+);
